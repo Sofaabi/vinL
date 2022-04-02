8ae806612c0b45638d4bfe2a5d937c6d0ba75f0b
Sofaabi <gay.catgirl.technologies@gmail.com>
2022-04-01 21:43:38 -0400

    Initialized project codebase using cargo init Added modules: os, os::process
    Added partial testing for os::process

---
+++ src/os/process.rs
@@ -0,0 +1,65 @@
+use std::{fs::{self, DirEntry}, io::{self, Write}};
+
+pub fn get_pid(process_name: &str) -> Result<Vec<String>, String>{
+    let proc_contents = fs::read_dir("/proc");
+    let proc_contents = match proc_contents {
+        Ok(dirs) => dirs,
+        _ => return Err(String::from("Could not access /proc."))
+    };
+
+    let processes = proc_contents.filter(is_process);
+    let processes_with_matching_name = processes.filter(|x| process_name_contains(x, process_name));
+    let correct_pids = processes_with_matching_name.map(|x| x.unwrap())
+                                                    .map(get_filename_str)
+                                                    .collect(); //We checked for Ok() twice now in the filters
+    Ok(correct_pids)
+    
+}
+
+fn get_filename_str(file: DirEntry) -> String {
+    let filepath = file.file_name();
+    let filename = filepath.to_str();
+    String::from(filename.unwrap())
+}
+
+fn process_name_contains(process: &Result<DirEntry, io::Error>, compared_string: &str) -> bool{
+    match process{
+        Ok(dir) => {
+            let pid = dir.file_name();
+            let pid = pid.to_str();
+            match pid {
+                Some(current_pid) => {
+                    let p_name = fs::read_to_string(format!("/proc/{}/comm", current_pid)).unwrap();
+                    p_name.contains(compared_string)
+                }
+                _ => false,
+            }
+        },
+        _ => false,
+    }
+}
+
+fn is_process(entry: &Result<DirEntry, io::Error>) -> bool {
+    match entry {
+        Ok(x) => {
+            let name = x.file_name();
+            let name = name.to_str();
+            match name {
+                Some(s) => s.parse::<i32>().is_ok(),
+                _ => false,
+            }
+        },
+        _ => false,
+    }
+}
+
+pub fn get_process_maps(pid: &str) -> Result<String, io::Error> {
+    let path = format!("/proc/{}/maps", pid);
+    fs::read_to_string(path)
+}
+
+pub fn write_to_file(path: &str, contents: Vec<u8>) -> Result<(), io::Error> {
+    let mut file = fs::File::create(path)?;
+    file.write_all(contents.as_slice())?;
+    Ok(())
+}
---
+++ tests/process.rs
@@ -0,0 +1,10 @@
+use vinL::os::process;
+
+//This test should ideally spawn a child process with a known name and pid, then find it. Currently this at least works fine on systemd running systems such as Arch.
+#[test]
+fn can_find_systemd(){
+    let x = process::get_pid("systemd");
+    let x = x.unwrap();
+    let x = &x[0];
+    assert!(x.contains("1"));
+}
